12aee8e13e892ee9b0e7e5a713d340dc80ed4335
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2022-08-13 22:44:51 -0700

    Add project files.

---
+++ Combo Processor Fix/Mod.cs
@@ -0,0 +1,18 @@
+﻿using PulsarModLoader;
+
+namespace Combo_Processor_Fix
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "1.0.0";
+
+        public override string Author => "Dragon";
+
+        public override string Name => "Combo Processor Fix";
+
+        public override string HarmonyIdentifier()
+        {
+            return $"{Author}.{Name}";
+        }
+    }
+}
---
+++ Combo Processor Fix/Patch.cs
@@ -0,0 +1,61 @@
+﻿using CodeStage.AntiCheat.ObscuredTypes;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Combo_Processor_Fix
+{
+    [HarmonyPatch(typeof(PLCPU), "Tick")]
+    class PowerPatch
+    {
+        static void Postfix(PLCPU __instance, ref float ___m_RequestPowerUsage_Percent)
+        {
+            if (__instance.CPUClass == ECPUClass.COMBO && (__instance.ShipStats == null || __instance.ShipStats.Ship == null || __instance.ShipStats.Ship.WarpChargeStage != EWarpChargeStage.E_WCS_PREPPING))
+            {
+                ___m_RequestPowerUsage_Percent *= 0.3125f;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(PLCPU), "AddStats")]
+    class FunctionPatch
+    {
+        static bool Prefix(PLCPU __instance, PLShipStats inStats)
+        {
+            if (__instance.CPUClass == ECPUClass.COMBO && (inStats.Ship == null || inStats.Ship.WarpChargeStage != EWarpChargeStage.E_WCS_PREPPING))
+            {
+                inStats.CyberDefenseRating += 0.375f * __instance.LevelMultiplier(0.75f, 1f) * (__instance.GetPowerPercentInput() / 0.3125f);
+                return false;
+            }
+            return true;
+        }
+    }
+
+    [HarmonyPatch(typeof(PLCPU), "ShowValueWithPowerLevelApplied")]
+    class StatLinePatchJP
+    {
+        static bool Prefix(PLCPU __instance, float value, ref string __result)
+        {
+            if (__instance.CPUClass == ECPUClass.COMBO && __instance.ShipStats != null && __instance.ShipStats.Ship != null && __instance.ShipStats.Ship.WarpChargeStage != EWarpChargeStage.E_WCS_PREPPING && !__instance.ShipStats.isPreview && !__instance.InCargoSlot())
+            {
+                //JP portion
+                __result = "0/" + value.ToString("0");
+                return false;
+            }
+            return true;
+        }
+    }
+    [HarmonyPatch(typeof(PLCPU), "ShowValueWithPowerLevelApplied_Decimal")]
+    class StatLinePatchCD
+    {
+        static bool Prefix(PLCPU __instance, float value, ref string __result)
+        {
+            if (__instance.CPUClass == ECPUClass.COMBO && __instance.ShipStats != null && __instance.ShipStats.Ship != null && __instance.ShipStats.Ship.WarpChargeStage != EWarpChargeStage.E_WCS_PREPPING && !__instance.ShipStats.isPreview && !__instance.InCargoSlot())
+            {
+                //CD portion
+                __result = (value * (__instance.GetPowerPercentInput() / 0.3125f)).ToString("0.0") + "/" + value.ToString("0.0");
+                return false;
+            }
+            return true;
+        }
+    }
+}
